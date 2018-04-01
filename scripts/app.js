d8cf6ef2dda97a6bf3a34b5cc3516dba34872fa8
Michael Cheng <mcheng.work@gmail.com>
2018-04-01 18:09:42 +0800

    First implementation of the site

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+    $('.slider').slider({ full_width: true, height: 700, interval: 5000 });
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+});
